fb04d6eec7059205767e1dc8d17e3404ed804ce7
xavioo <rucahoxavi@gmail.com>
2021-10-18 11:17:53 +0200

    reassigning

--- java.java
+++ java.java
@@ -117,3 +117,30 @@ public class Store {
     
   }
 }
+
+//Reassigning Instance Fields
+
+public class Store {
+  // instance fields
+  String productType;
+  double price;
+  
+  // constructor method
+  public Store(String product, double initialPrice) {
+    productType = product;
+    price = initialPrice;
+  }
+  // increase price method
+  public void increasePrice(double priceToAdd){
+  double newPrice = price  + priceToAdd;
+  price = newPrice; 
+  }
+  // main method
+  public static void main(String[] args) {
+ Store lemonadeStand = new Store("Lemonade", 3.75);
+  System.out.println(lemonadeStand.price);
+  lemonadeStand.increasePrice(1.5);
+  System.out.println(lemonadeStand.price);
+    
+  }
+}
