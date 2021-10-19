db9d0020103c3f48cd1507765da043403d6b72e3
xavioo <rucahoxavi@gmail.com>
2021-10-19 06:56:32 +0200

    length

--- java.java
+++ java.java
@@ -439,4 +439,35 @@ public class Newsfeed {
   }
 }
 
+//LEARN JAVA: ARRAYS Length
+
+import java.util.Arrays;
+
+public class Newsfeed {
+  
+  String[] topics = {"Opinion", "Tech", "Science", "Health"};
+  int[] views = {0, 0, 0, 0};
+  
+  public Newsfeed(){
+
+  }
+    
+  public String[] getTopics(){
+    return topics;
+  }
+  
+  public int getNumTopics(){
+    return topics.length;
+    
+  }
+  
+  public static void main(String[] args){
+    Newsfeed sampleFeed = new Newsfeed();
+    
+    System.out.println("The number of topics is "+ sampleFeed.getNumTopics());
+   
+  }
+}
+
+
 
