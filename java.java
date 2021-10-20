f323d9aa8e1afa75d8ce18933c357a4201feb192
xavioo <rucahoxavi@gmail.com>
2021-10-20 05:12:15 +0200

    JAVA I

--- java.java
+++ java.java
@@ -986,3 +986,40 @@ System.out.println(myCalculator.modulo (5, 2));
 
 } 
 }
+
+//JAVA I Build A Droid
+
+public class Droid{
+ String name;
+ int batteryLevel;
+
+ public Droid (String droidName) {
+  name = droidName;
+  batteryLevel = 100;
+    }
+public String toString(){
+    return ("Hello, I'm the droid:" + name);
+  }  
+
+ public void performTask(String task){
+    System.out.println(name + " is performing  task " + task);
+    batteryLevel -=10;
+      }  
+public void energyReport(){
+        System.out.println("The current battery level is: " + batteryLevel);
+        }
+
+  public void energyTransfer(){    
+    System.out.println("The current battery level is: " + batteryLevel);
+  }    
+
+ public static void main(String[] args) {
+  Droid codey  = new Droid("Codey");
+  System.out.println(codey);
+
+  codey.performTask ("dancing.");
+   codey.energyReport();
+    codey.energyTransfer();
+ 
+  }
+}
