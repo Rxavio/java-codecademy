a618b5b887129841dfc930cb3d8ece9e43736175
xavioo <rucahoxavi@gmail.com>
2021-10-22 19:38:53 +0200

    LOOPS For-Each Loops

--- java.java
+++ java.java
@@ -1120,3 +1120,33 @@ class Numbers {
     }
   }
 }
+
+//LEARN JAVA: LOOPS For-Each Loops
+import java.util.ArrayList;
+
+class MostExpensive {
+  
+  public static void main(String[] args) {
+    
+    ArrayList<Double> expenses = new ArrayList<Double>();
+    expenses.add(74.46);
+    expenses.add(63.99);
+    expenses.add(10.57);
+    expenses.add(81.37);
+    
+    double mostExpensive = 0;
+    
+    // Iterate over expenses
+    for (double expense : expenses) {
+      
+      if (expense > mostExpensive) {
+        mostExpensive = expense;
+      }
+      
+    }
+    
+    System.out.println(mostExpensive);
+    
+  }
+  
+}
