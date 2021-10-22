d84df86bcf9c12978fbb208499c50dc354379c02
xavioo <rucahoxavi@gmail.com>
2021-10-22 19:51:40 +0200

    STRING METHODS length

--- java.java
+++ java.java
@@ -1224,3 +1224,18 @@ public class HelloWorld {
   }
   
 }
+
+//STRING METHODS length()
+public class TweetCounter {
+  
+	public static void main(String[] args) {
+    
+String tweet = "Liz Lemon, ninjas are kind of cool... I just dont know any personally. Get on that.";  
+
+    // What's the character count?
+    System.out.println(tweet.length());
+    
+    
+  }
+  
+}
