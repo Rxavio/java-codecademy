d0596fe01e26087ab64660a87fd8bdb6127c8488
xavioo <rucahoxavi@gmail.com>
2021-10-22 19:20:35 +0200

    java 2 loops

--- java.java
+++ java.java
@@ -1023,3 +1023,27 @@ public void energyReport(){
  
   }
 }
+//LEARN JAVA: LOOPS
+
+// Importing the Random library
+import java.util.Random;
+
+class LuckyFive {
+  
+  public static void main(String[] args) {
+    
+    // Creating a random number generator
+    Random randomGenerator = new Random();
+    
+    // Generate a number between 1 and 6
+    int dieRoll = randomGenerator.nextInt(6) + 1;
+
+    // Repeat while roll isn't 5
+    while (dieRoll != 5) {
+  // code goes here
+    System.out.println(dieRoll);
+		dieRoll = randomGenerator.nextInt(6) + 1;
+ }
+ }
+}
+
